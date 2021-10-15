e477f17cb0013ee1e636401ce14d8936cc7d8688
Christian Aurich <christian.aurichzm@gmail.com>
2021-10-15 02:18:51 -0300

    Add initial backend configuration

---
+++ backend/src/main/kotlin/br/com/christian/developersplace/DevelopersPlaceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.christian.developersplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevelopersPlaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevelopersPlaceApplication>(*args)
+}
---
+++ backend/src/test/kotlin/br/com/christian/developersplace/DevelopersPlaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.christian.developersplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevelopersPlaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
