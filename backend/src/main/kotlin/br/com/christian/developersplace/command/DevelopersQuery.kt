76754291fb1b40c15160238a7e843ea71c3e1b6d
Christian Aurich <christian.aurichzm@gmail.com>
2021-10-22 04:11:40 -0300

    Optional filters in DevelopersQuery

--- backend/src/main/kotlin/br/com/christian/developersplace/command/DevelopersQuery.kt
+++ backend/src/main/kotlin/br/com/christian/developersplace/command/DevelopersQuery.kt
@@ -4,15 +4,19 @@ import br.com.christian.developersplace.graphql.input.DeveloperQueryInput
 import br.com.christian.developersplace.model.Developer
 import br.com.christian.developersplace.model.QDeveloper.developer
 import br.com.christian.developersplace.repository.DAO
+import com.querydsl.jpa.impl.JPAQuery
 import org.springframework.stereotype.Repository
 
 @Repository
 class DevelopersQuery(private val dao: DAO) {
 
     fun execute(input: DeveloperQueryInput): List<Developer> {
-        return this.dao.queryFactory()
-            .selectFrom(developer)
-            .where(developer.name.containsIgnoreCase(input.name))
-            .fetch()
+        val query: JPAQuery<Developer> = this.dao.queryFactory().selectFrom(developer)
+
+        input.name?.let {
+            query.where(developer.name.containsIgnoreCase(input.name))
+        }
+
+        return query.fetch()
     }
 }
